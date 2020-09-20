74ffc4beaf90614ce0abd2dc83a9e6c23865b81f
Freyja <fjvalkyrie@gmail.com>
2020-09-20 15:23:24 -0500

    Fix bad elisp

--- abyss-hicontrast-theme.el
+++ abyss-hicontrast-theme.el
@@ -29,8 +29,8 @@
        (abyss-hicontrast/hl-line "#748074")
        (abyss-hicontrast/hilite "#dd5542")
        (abyss-hicontrast/white "#ffffff")
-       (abyss-hicontrast/green "#00ff00"))
-       (abyss/magenta "#ff00ff")
+       (abyss-hicontrast/green "#00ff00")
+       (abyss/magenta "#ff00ff"))
   (custom-theme-set-faces
    `abyss-hicontrast
    `(bold ((t (:bold t))))
